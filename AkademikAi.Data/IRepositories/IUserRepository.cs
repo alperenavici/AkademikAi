50d2a703cfcd259e35be569fd9c2fa3de2bd0bb4
Alperen Avci <147971625+alperenavici@users.noreply.github.com>
2025-07-24 18:15:58 +0300

    upd rp

--- AkademikAi.Data/IRepositories/IUserRepository.cs
+++ AkademikAi.Data/IRepositories/IUserRepository.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AkademikAi.Entity.Entites;
+using AkademikAi.Entity.Enums;
 
 namespace AkademikAi.Data.IRepositories
 {
     public interface IUserRepository
     {
+        Task<Users> GetUserByIdAsync(Guid UserId);
+        Task<List<Users>> GetByUserRoleAsync(UserRole userRole);
+        Task<Users> GetByEmailAsync(string email);
+        Task<List<Users>> GetAllUsersAsync();
+        Task<Users> GetUserByEmailAsync(string email);
+        Task<List<Users>> GetUsersByPhoneAsync(string phone);
+        Task<List<Users>> GetUsersByNameAsync(string name);
+        Task<List<Users>> GetUsersBySurnameAsync(string name,string surname);
     }
 }
